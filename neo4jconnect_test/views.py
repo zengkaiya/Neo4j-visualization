d779be6dacacb70e03b77d991deba94c4e9f3bb1
HHgzs <3166237657@qq.com>
2024-09-07 23:58:48 +0800

    search

--- neo4jconnect_test/views.py
+++ neo4jconnect_test/views.py
@@ -3,14 +3,11 @@ import requests
 from django.shortcuts import render
 from .func import *
 
-
-# 查询所有节点和关系
-
+model = Model()
 
 
-
-
-last_request = None
+che_data = []
+dis_data = []
 
 # 视图函数
 def index(request):
@@ -24,6 +21,10 @@ def index(request):
             property_name = request.POST.get('property_name')
             property_value = request.POST.get('property_value')
 
+            che_data = []
+            dis_data = []
+            links_data = []
+            neo4j_search = []
 
             che_id_ans = model.find_chemical_by_id(property_value)
             dis_id_ans = model.find_disease_by_id(property_value)
@@ -32,32 +33,56 @@ def index(request):
 
             if che_id_ans == None and che_name_ans == None and dis_id_ans == None and dis_name_ans == None:
                 ctx = {'title': '数据库中暂未添加该实体'}
-                return render(request, 'index.html', {'neo4j_data': neo4j_data, 'ctx': ctx})
+                return render(request, 'index.html', {'ctx': ctx})
             
-            elif che_id_ans != None or che_name_ans != None:
-                links_ans = model.find_diseases_associated_with_chemical(property_value)
-            elif dis_id_ans != None or dis_name_ans != None:
-                links_ans = model.find_chemicals_associated_with_disease(property_value)
-            else:
-                links_ans = []
-            
-            search_data = [
-            {
-                'data': [
-                    {'name': 'SearchNode1', 'category': 0, 'des': 'This is SearchNode 1'},
-                ],
-                'links': [
-                    {'source': 'SearchNode1', 'target': 'SearchNode2', 'name': 'SearchRelation'},
-                ]
-            }
-        ]
+
+            elif che_id_ans != None or len(che_name_ans) != 0:
+
+                if che_id_ans != None:
+                    che_data.append(che_id_ans)
+                else:
+                    che_data.append(che_name_ans[0])
+
+                che_id = che_data[0].chemical_id
+                links_ans = model.find_diseases_associated_with_chemical(che_id)
+                if len(links_ans) != 0:
+                    for link in links_ans:
+                        links_data.append({'source': che_id, 'target': link.disease_id, 'name': 'associated_with'})
+                        dis_data.append(model.find_disease_by_id(link.disease_id))
 
 
+            elif dis_id_ans != None or len(dis_name_ans) != 0:
 
-            
-            print(f'node/edge: {node_or_edge}, property_name: {property_name}, property_value: {property_value}')
+                if dis_id_ans != None:
+                    dis_data.append(dis_id_ans)
+                else:
+                    dis_data.append(dis_name_ans[0])
 
-            pass
+                dis_id = dis_data[0].disease_id
+                links_ans = model.find_chemicals_associated_with_disease(dis_id)
+                if len(links_ans) != 0:
+                    for link in links_ans:
+                        links_data.append({'source': dis_id, 'target': link.chemical_id, 'name': 'associated_with'})
+                        che_data.append(model.find_chemical_by_id(link.chemical_id))
+
+            neo4j_search_data = []
+
+            if len(che_data) != 0:
+                for che in che_data:
+                    neo4j_search_data.append({'name': che.chemical_id, 'category': 0, 'des': che.name})
+
+            if len(dis_data) != 0:
+                for dis in dis_data:
+                    neo4j_search_data.append({'name': dis.disease_id, 'category': 1, 'des': dis.name})
+
+            neo4j_search = {'data': neo4j_search_data, 'links': links_data}
+            neo4j_search = [neo4j_search]
+            print(neo4j_search)
+
+            ctx = None
+            return render(request, 'index.html', {'neo4j_data': neo4j_search, 'ctx': ctx})
+
+
 
         elif action == 'add':
             pass
@@ -68,68 +93,5 @@ def index(request):
         elif action == 'update':
             pass
 
-        node_or_edge = request.POST.get('node_or_edge')
-        property_name = request.POST.get('property_name')
-        property_value = request.POST.get('property_value')
-
-        print(f'node/edge: {node_or_edge}, property_name: {property_name}, property_value: {property_value}')
-
-        if node_or_edge == 'node':
-            if property_name != '':
-                search_neo4j_data = model.search_node_by_property(property_name, property_value)
-
-            elif property_name == '':
-                search_neo4j_node_d = model.find_disease_by_name
-                search_neo4j_data = model.search_node_by_name(property_value)
-
-        elif node_or_edge == 'edge':
-            pass
-
-        else:
-            return render(request, 'index.html', {'ctx': ctx})
-
-
-        
-    #     search_neo4j_data = [
-    #         {
-    #             'data': [
-    #                 {'name': 'SearchNode1', 'category': 0, 'des': 'This is SearchNode 1'},
-    #                 {'name': 'SearchNode2', 'category': 1, 'des': 'This is SearchNode 2'},
-    #             ],
-    #             'links': [
-    #                 {'source': 'SearchNode1', 'target': 'SearchNode2', 'name': 'SearchRelation'},
-    #             ]
-    #         }
-    #     ]
-    #     if search_neo4j_data == 0:
-    #         print(-1)
-    #         ctx = {'title': '数据库中暂未添加该实体'}
-    #         neo4j_data = search_all()
-    #         return render(request, 'index.html', {'neo4j_data': neo4j_data, 'ctx': ctx})
-    #     else:
-    #         # 走的是这里
-    #         neo4j_data = search_all()
-    #         print(2)
-    #         neo4j_data = [
-    #             {
-    #                 'data': [
-    #                     {'name': 'Node1', 'category': 0, 'des': 'This is Node 1'},
-    #                     {'name': 'Node2', 'category': 1, 'des': 'This is Node 2'},
-    #                     {'name': 'Node3', 'category': 2, 'des': 'This is Node 3'},
-    #                 ],
-    #                 'links': [
-    #                     {'source': 'Node1', 'target': 'Node2', 'name': 'Relation1'},
-    #                     {'source': 'Node2', 'target': 'Node3', 'name': 'Relation2'},
-    #                 ]
-    #             }
-    #         ]
-    #         neo4j_data = search_neo4j_data
-    #         ctx = None
-    #         return render(request, 'index.html',
-    #                       {'neo4j_data': json.dumps(neo4j_data),
-    #                        'search_neo4j_data': json.dumps(search_neo4j_data),
-    #                        'ctx': ctx})
-
-    neo4j_data = []
     
-    return render(request, 'index.html', {'neo4j_data': neo4j_data, 'ctx': ctx})
+    return render(request, 'index.html', {'ctx': ctx})
